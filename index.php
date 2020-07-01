1c6bc24886f4cd22d08b3986befb683d58129335
amirah2603 <60610870+amirah2603@users.noreply.github.com>
2020-07-02 01:48:16 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	<h1>Welcome Back, Amirah.</h1>
 
 	<div class="btn-group-vertical">
-    	<input type=button onClick=location.href="<?php echo ROOT_URL; ?>" class="btn" value="CALCULATE BMI">
+    	<input type=button onClick=location.href="<?php echo ROOT_URL; ?>calculate" class="btn" value="CALCULATE BMI">
     	<input type=button onClick=location.href="<?php echo ROOT_URL; ?>progress_update" class="btn" value="PERFORMANCE">
     	<input type=button onClick=location.href="<?php echo ROOT_URL; ?>advice" class="btn" value="NUTRITIONAL ADVICE">
 	</div>
