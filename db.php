8ce710baf7eaf08dd6131d866403ae09156f2570
amirah2603 <60610870+amirah2603@users.noreply.github.com>
2020-07-01 23:50:39 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
 
-$host = "";
-$user = "";
-$password = "";
-$dbname = "";
-$port = "";
+$host = "ec2-34-197-188-147.compute-1.amazonaws.com";
+$user = "xaiwgzulsgxtkb";
+$password = "fc7a410c7df6fb94b409aa0d78e3f2fb15440e42f75b694fbb2a966e448ad8d6";
+$dbname = "danoh2mjnt7j0s";
+$port = "5432";
 
 try{
   //Set DSN data source name
