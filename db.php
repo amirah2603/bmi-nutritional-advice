3cf1f229de66bd194255d2732e0f5100ec9c8a2a
amirah2603 <60610870+amirah2603@users.noreply.github.com>
2020-07-02 10:08:11 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,26 @@
 <?php
 
-$host = "ec2-34-197-188-147.compute-1.amazonaws.com";
-$user = "xaiwgzulsgxtkb";
-$password = "fc7a410c7df6fb94b409aa0d78e3f2fb15440e42f75b694fbb2a966e448ad8d6";
-$dbname = "danoh2mjnt7j0s";
-$port = "5432";
+$db = parse_url(getenv("postgres://xaiwgzulsgxtkb:fc7a410c7df6fb94b409aa0d78e3f2fb15440e42f75b694fbb2a966e448ad8d6@ec2-34-197-188-147.compute-1.amazonaws.com:5432/danoh2mjnt7j0"));
 
-try{
-  //Set DSN data source name
-    $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["ec2-34-197-188-147.compute-1.amazonaws.com"],
+    $db["5432"],
+    $db["xaiwgzulsgxtkb"],
+    $db["fc7a410c7df6fb94b409aa0d78e3f2fb15440e42f75b694fbb2a966e448ad8d6"],
+    ltrim($db["composer.json"], "/")
+));
+
+
+// $host = "ec2-34-197-188-147.compute-1.amazonaws.com";
+// $user = "xaiwgzulsgxtkb";
+// $password = "fc7a410c7df6fb94b409aa0d78e3f2fb15440e42f75b694fbb2a966e448ad8d6";
+// $dbname = "danoh2mjnt7j0s";
+// $port = "5432";
+
+// try{
+//   //Set DSN data source name
+//     $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
 
 
   //create a pdo instance
