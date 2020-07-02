d3e9601ff955c7249fe5b2208ca051c26a2b5a96
amirah2603 <60610870+amirah2603@users.noreply.github.com>
2020-07-02 08:21:35 +0800

    Update advice.php

--- advice.php
+++ advice.php
@@ -22,12 +22,12 @@
   		<div class="col my-col">
   			<div class="centered">
   				<?php
-				$sql = "SELECT user_id, user_bmi, created_at FROM bmi WHERE timestamp = (SELECT MAX(created_at) FROM bmi);";
-				$result = $pdo->prepare($sql);
-// 				$result = $conn-> query($sql);
-				$result->execute();
-// 				$result = $result-> fetch_assoc(); 
-				$details = $result->fetch();
+// 				$sql = "SELECT user_id, user_bmi, created_at FROM bmi WHERE timestamp = (SELECT MAX(created_at) FROM bmi);";
+// 				$result = $pdo->prepare($sql);
+// // 				$result = $conn-> query($sql);
+// 				$result->execute();
+// // 				$result = $result-> fetch_assoc(); 
+// 				$details = $result->fetch();
 				// $BMI = $result["length"];
 // 				$BMI = $result["user_bmi"];
  				$BMI = 19;
