842ed57df8298e1e1484f81b035d578ebf5575f8
amirah2603 <60610870+amirah2603@users.noreply.github.com>
2020-07-02 11:57:43 +0800

    Update advice.php

--- advice.php
+++ advice.php
@@ -22,7 +22,7 @@
   		<div class="col my-col">
   			<div class="centered">
   				<?php
-				$sql = 'SELECT * FROM bmi ORDER BY user_id)';
+				$sql = 'SELECT * FROM bmi)';
   				$stmt = $pdo->prepare($sql);
   				$stmt->execute();
 				$result = $stmt->fetch(PDO::FETCH_OBJ);
